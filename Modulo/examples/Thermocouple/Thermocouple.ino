912957ffe0d5fcba82e821056bf7899227efc7d2
Erin Tomson <erin@integerlabs.net>
2015-02-15 22:16:54 -0800

    Updated to show the current and a target temperature and change the target temp when the knob is rotated

--- Modulo/examples/Thermocouple/Thermocouple.ino
+++ Modulo/examples/Thermocouple/Thermocouple.ino
@@ -3,31 +3,45 @@
 
 MiniDisplayModule display;
 ThermocoupleModule thermocouple;
-  
+KnobModule knob;
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
 }
 
+int getSetTemp() {
+    return knob.getPosition()*5 + 325;
+}
+
 void loop() {
   // put your main code here, to run repeatedly:
-  delay(500);
-  
-  int16_t temp = thermocouple.getTemperature(); 
-  Serial.println(temp);
 
+  int temp = thermocouple.getTemperatureF();
+  int setTemp = getSetTemp();
+  
   display.setCursor(10,10);
   display.fillScreen(0);
-  display.println("Thermocouple");
+  display.setTextSize(1);
+  display.println("Current     Target\n");
+  display.setTextSize(2);
+  
+  display.setCursor(0,40);
+  display.print((int)temp);
+  display.print("\xF7" "F");
 
-  display.print("   Device ID: 0x");
-  display.println(thermocouple.getDeviceID(), HEX);
+  display.setCursor(display.width()/2,40);
+  display.print(setTemp);
+  display.print("\xF7" "F");
 
-  display.print("   Address : ");
-  display.println(thermocouple.getAddress());
+  if (abs(temp-setTemp) < 10) {
+      knob.setColor(0,255,0);
+  } else if (temp > getSetTemp()) {
+     knob.setColor(255,0,0);
+  } else {
+     knob.setColor(0,0,255);
+  }
 
-  display.print("   Temperature: ");
-  display.println(temp/10.0);
   display.display();
     
 }
