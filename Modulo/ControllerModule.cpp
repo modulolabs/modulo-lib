8da83002d329a8cb0d85ab3ee5af56413c9d75be
Erin Tomson <erin@integerlabs.net>
2015-02-27 19:59:22 -0800

    Added API for controller I/O pins.

--- Modulo/ControllerModule.cpp
+++ Modulo/ControllerModule.cpp
@@ -1,15 +1,20 @@
 #include "ControllerModule.h"
 #include "Modulo.h"
 
-#define FUNCTION_READ_TEMPERATURE_PROBE 0
 
+#define FUNCTION_SET_PIN_DIRECTION 0
+#define FUNCTION_GET_DIGITAL_INPUT 1
+#define FUNCTION_SET_DIGITAL_OUTPUT 2
+#define FUNCTION_SET_PWM_OUTPUT 3
+#define FUNCTION_GET_ANALOG_INPUT 4
+#define FUNCTION_READ_TEMPERATURE_PROBE 5
 
 ControllerModule::ControllerModule() :
     Module("co.modulo.controller", 0 /*deviceID*/)
 {
 }
 
-float ControllerModule::readTemperatureProbe(uint8_t pin)
+float ControllerModule::readTemperatureInput(uint8_t pin)
 {
     uint8_t receiveData[2] = {0,0};
     if (!moduloTransfer(getAddress(), FUNCTION_READ_TEMPERATURE_PROBE, &pin, 1, receiveData, 2)) {
@@ -18,3 +23,41 @@ float ControllerModule::readTemperatureProbe(uint8_t pin)
     int16_t tenths = (receiveData[0] | receiveData[1] << 8);
     return tenths/10.0;
 }
+
+void ControllerModule::setPinDirection(uint8_t pin, bool output, bool pullup)
+{
+    uint8_t data = (pin << 2) | (pullup << 1) | output;
+    moduloTransfer(getAddress(), FUNCTION_SET_PIN_DIRECTION, &data, 1, 0, 0);
+}
+
+bool ControllerModule::getDigitalInput(uint8_t pin) {
+    uint8_t receiveData = 0;
+    if (!moduloTransfer(getAddress(), FUNCTION_GET_DIGITAL_INPUT, &pin, 1, &receiveData, 1)) {
+        return false;
+    }
+    return receiveData & (1 << pin);
+}
+
+
+void ControllerModule::setDigitalOutput(uint8_t pin, bool value) {
+    uint8_t data = (pin << 1) | value;
+    moduloTransfer(getAddress(), FUNCTION_SET_DIGITAL_OUTPUT, &data, 1, 0, 0);
+}
+
+
+void ControllerModule::setPWMOutput(uint8_t pin, float value) {
+    uint8_t intValue = 255*max(0, min(1, value));
+    uint8_t data[] = {pin, intValue};
+    moduloTransfer(getAddress(), FUNCTION_SET_PWM_OUTPUT, data, 2, 0, 0);
+}
+
+
+float ControllerModule::getAnalogInput(uint8_t pin) {
+    uint8_t value;
+    if (!moduloTransfer(getAddress(), FUNCTION_GET_ANALOG_INPUT, &pin, 1, &value, 1)) {
+        return 0.0;
+    }
+    return value/255.0;
+}
+
+
--- Modulo/ControllerModule.h
+++ Modulo/ControllerModule.h
@@ -9,10 +9,36 @@ class ControllerModule : public Module {
 public:
     ControllerModule();
 
-    /// Get the temperature in celsius from a 1-wire temperature sensor connected to the specified pin.
-    float readTemperatureProbe(uint8_t pin);
+    /// Configure the specified \a pin to behave as an input or an output.
+    /// If configured as an input, also either enable or disable the pullup
+    /// resistor on the specified pin.
+    ///
+    /// If the pullup resistor is disabled (the default) and a pin is
+    /// not connected, reading from the pin may not return a consistent
+    /// value. Enabling the pull-up causes the pin to return true when
+    /// disconnected.
+    void setPinDirection(uint8_t pin, bool output, bool pullup=true);
 
- private:
+    /// Return the value from a specified pin, either HIGH (true) or LOW (false).
+    bool getDigitalInput(uint8_t pin);
+
+    /// Set the output value of the specified \a pin. The voltage on the
+    /// pin will be driven to 5V (true) or 0V (false). The pin must be
+    /// configured as an output with setPinDirection.
+    void setDigitalOutput(uint8_t pin, bool value);
+
+    /// Enable pulse width modulation on the specified \a pin. The
+    /// provided value represents the precentage of time that the
+    /// output will be high. 
+    void setPWMOutput(uint8_t pin, float value);
+
+    /// Read the analog voltage on the specified pin. This will return a
+    /// value between 0 and 1 representing a voltage between 0 and 5V.
+    float getAnalogInput(uint8_t pin);
+
+    /// Get the temperature in celsius from a 1-wire temperature
+    /// sensor connected to the specified pin.
+    float readTemperatureInput(uint8_t pin);
 };
 
 #endif
