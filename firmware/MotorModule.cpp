0bcc0ef64ca40e1cb7b5428ef103fbf4b83cdb45
Erin Tomson <erin@integerlabs.net>
2015-04-16 10:16:24 -0700

    Implemented motor module

--- firmware/MotorModule.cpp
+++ firmware/MotorModule.cpp
@@ -1,1 +1,60 @@
 #include "MotorModule.h"
+
+#include "Arduino.h"
+#include "Modulo.h"
+
+static const uint8_t _FunctionSetValue = 0;
+static const uint8_t _FunctionGetCurrent = 1;
+static const uint8_t _FunctionSetEnabled = 2;
+static const uint8_t _FunctionSetFrequency = 3;
+
+MotorModule::MotorModule() : Module("co.modulo.motor") {
+
+}
+
+
+MotorModule::MotorModule(uint16_t deviceID) : 
+    Module("co.modulo.motor", deviceID) {
+}
+
+
+void MotorModule::setChannel(uint8_t channel, float amount) {
+    uint16_t intValue = constrain(amount, 0, 1)*0xFFFF;
+    uint8_t data[] = {channel, intValue & 0xFF, intValue >> 8};
+    moduloTransfer(getAddress(), _FunctionSetValue, data, 3, 0, 0);
+} 
+
+
+void MotorModule::setMotorA(float value) {
+    if (value > 0) {
+        setChannel(0, value);
+        setChannel(1, 0);
+    } else {
+        setChannel(0, 0);
+        setChannel(1, -value);
+    }
+}
+
+
+void MotorModule::setMotorB(float value) {
+    if (value > 0) {
+        setChannel(2, value);
+        setChannel(3, 0);
+    } else {
+        setChannel(2, 0);
+        setChannel(3, -value);
+    }
+}
+  
+
+void MotorModule::setEnableA(bool enable) {
+    uint8_t dataToSend[] = {0, enable};
+    moduloTransfer(getAddress(), _FunctionSetEnabled, dataToSend, 2, 0, 0);
+
+}
+  
+
+void MotorModule::setEnableB(bool enable) {
+    uint8_t dataToSend[] = {2, enable};
+    moduloTransfer(getAddress(), _FunctionSetEnabled, dataToSend, 2, 0, 0);
+}
--- firmware/MotorModule.h
+++ firmware/MotorModule.h
@@ -5,44 +5,31 @@
 
 /// A driver for DC Motors, Stepper Motor, and other high current devices.
 class MotorModule : public Module {
+public:
+    /// Use the first Motor  that's not already in use
+    MotorModule();
 
-  /// Set a single channel (0-3) to the specified amount, between 0 and 1.
-  void setChannelOutput(uint8_t channel, float amount);
-  
-  /// Set the motor output A to the specified amount, between -1 and 1.
-  /// Enables A0 and A1 if necessary.
-  void setMotorA(float amount);
-  
-  /// Set the motor output B to the specified amount, between -1 and 1.
-  /// Enables B0 and B1 if necessary.
-  void setMotorB(float amount);
-  
-  /// Set whether output drivers are enabled on outputs A0 and A1
-  void setEnableA(bool enable);
-  
-  /// Set whether output drivers are enabled on outputs B0 and B1
-  void setEnableB(bool enable);
-  
-  /// Set the PWM frequency for A0 and A1
-  void setFrequencyA(uint16_t frequency);
-  
-  /// Set the PWM frequency for B0 and B1
-  void setFrequencyB(uint16_t frequency);
-  
-  /// Set the stepper motor rotation speed
-  void setStepperSpeed(float degPerSecond);
+    /// Use the Motor with the provided deviceID
+    MotorModule(uint16_t deviceID);
 
-  /// Step by the specified number of steps, either forward or back
-  void beginStepper(int16_t steps);
+    /// Set a single channel (0-3) to the specified amount, between 0 and 1.
+    void setChannel(uint8_t channel, float amount);
+    
+    /// Set the motor output A to the specified amount, between -1 and 1.
+    /// Enables A0 and A1 if necessary.
+    void setMotorA(float amount);
+    
+    /// Set the motor output B to the specified amount, between -1 and 1.
+    /// Enables B0 and B1 if necessary.
+    void setMotorB(float amount);
+    
+    /// Set whether output drivers are enabled on outputs A0 and A1
+    void setEnableA(bool enable);
+    
+    /// Set whether output drivers are enabled on outputs B0 and B1
+    void setEnableB(bool enable);
+
   
-  /// Stop the stepper motor. This will leave the motor engaged
-  void stopStepper();
-  
-  /// Disable the stepper motor, allowing it to turn freely.
-  void disableStepper();
-  
-  /// Return the number of steps completed since the last call to step()
-  int16_t getStepsCompleted();
 };
 
 #endif
