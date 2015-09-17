7b89ddf07c37ed6b733cfa494a3d2b0c701d223c
Erin Tomson <erin@integerlabs.net>
2015-09-17 11:18:24 -0700

    Whitespace.

--- firmware/MotorModule.cpp
+++ firmware/MotorModule.cpp
@@ -1,7 +1,16 @@
 #include "MotorModule.h"
 
-#include "Arduino.h"
 #include "Modulo.h"
+
+static float constrain(float v, float min, float max) {
+    if (v < min) {
+        return min;
+    }
+    if (v < max) {
+        return max;
+    }
+    return v;
+}
 
 static const uint8_t _FunctionSetValue = 0;
 static const uint8_t _FunctionGetCurrent = 1;
@@ -9,21 +18,17 @@ static const uint8_t _FunctionSetEnabled = 2;
 static const uint8_t _FunctionSetFrequency = 3;
 
 MotorModule::MotorModule() : Module("co.modulo.motor") {
-
 }
 
-
-MotorModule::MotorModule(uint16_t deviceID) : 
+MotorModule::MotorModule(uint16_t deviceID) :
     Module("co.modulo.motor", deviceID) {
 }
-
 
 void MotorModule::setChannel(uint8_t channel, float amount) {
     uint16_t intValue = constrain(amount, 0, 1)*0xFFFF;
     uint8_t data[] = {channel, intValue & 0xFF, intValue >> 8};
     _transfer(_FunctionSetValue, data, 3, 0, 0);
-} 
-
+}
 
 void MotorModule::setMotorA(float value) {
     if (value > 0) {
@@ -35,7 +40,6 @@ void MotorModule::setMotorA(float value) {
     }
 }
 
-
 void MotorModule::setMotorB(float value) {
     if (value > 0) {
         setChannel(2, value);
@@ -45,14 +49,12 @@ void MotorModule::setMotorB(float value) {
         setChannel(3, -value);
     }
 }
-  
 
 void MotorModule::setEnableA(bool enable) {
     uint8_t dataToSend[] = {0, enable};
     _transfer(_FunctionSetEnabled, dataToSend, 2, 0, 0);
 
 }
-  
 
 void MotorModule::setEnableB(bool enable) {
     uint8_t dataToSend[] = {2, enable};
