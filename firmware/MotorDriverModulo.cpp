6e14f252817f9329688584f30ae427591c7811e4
Erin Tomson <erin@integerlabs.net>
2015-11-11 12:06:29 -0800

    Added stepper motor support.

--- firmware/MotorDriverModulo.cpp
+++ firmware/MotorDriverModulo.cpp
@@ -1,5 +1,6 @@
 #include "MotorDriverModulo.h"
 #include "Modulo.h"
+#include "Arduino.h"
 
 static float _constrain(float v, float min, float max) {
     if (v < min) {
@@ -16,17 +17,30 @@ static const uint8_t _FunctionSetValue = 0;
 static const uint8_t _FunctionSetEnabled = 1;
 static const uint8_t _FunctionSetFrequency = 2;
 static const uint8_t _FunctionSetCurrentLimit = 3;
+static const uint8_t _FunctionSetStepperSpeed = 4;
+static const uint8_t _FunctionGetStepperPosition = 5;
+static const uint8_t _FunctionSetStepperTarget = 6;
+static const uint8_t _FunctionAddStepperOffset = 7;
 
-MotorDriverModulo::MotorDriverModulo() : BaseModulo("co.modulo.motor") {
+static const uint8_t _EventPositionReached = 0;
+static const uint8_t _EventFaultChanged = 1;
+
+MotorDriverModulo::MotorDriverModulo() : BaseModulo("co.modulo.motor"),
+    _fault(false), _stepperOffset(0), _usPerStep(5000), _microsteps(256), _minMicrostepDuration(1000),
+    _positionReachedCallback(NULL), _faultChangedCallback(NULL)
+{
 }
 
 MotorDriverModulo::MotorDriverModulo(uint16_t deviceID) :
-    BaseModulo("co.modulo.motor", deviceID) {
+    BaseModulo("co.modulo.motor", deviceID),
+    _fault(false), _stepperOffset(0), _usPerStep(5000), _microsteps(256), _minMicrostepDuration(1000),
+    _positionReachedCallback(NULL), _faultChangedCallback(NULL)
+{
 }
 
 void MotorDriverModulo::setChannel(uint8_t channel, float amount) {
     uint16_t intValue = _constrain(amount, 0, 1)*0xFFFF;
-    uint8_t data[] = {channel, intValue & 0xFF, intValue >> 8};
+    uint8_t data[] = {channel, static_cast<uint8_t>(intValue), static_cast<uint8_t>(intValue >> 8)};
     _transfer(_FunctionSetValue, data, 3, 0, 0);
 }
 
@@ -50,8 +64,8 @@ void MotorDriverModulo::setMotorB(float value) {
     }
 }
 
-void MotorDriverModulo::setEnable(bool enable) {
-    uint8_t dataToSend[] = {enable};
+void MotorDriverModulo::setMode(Mode mode) {
+    uint8_t dataToSend[] = {static_cast<uint8_t>(mode)};
     _transfer(_FunctionSetEnabled, dataToSend, 1, 0, 0);
 }
 
@@ -60,3 +74,126 @@ void MotorDriverModulo::setCurrentLimit(float limit) {
     uint8_t dataToSend[] = {limit*64};
     _transfer(_FunctionSetCurrentLimit, dataToSend, 1, 0, 0);
 }
+
+void MotorDriverModulo::setStepperSpeed(float stepsPerSecond) {
+    setStepperRate(1000000/stepsPerSecond);
+}
+
+void MotorDriverModulo::setStepperRate(uint32_t usPerStep) {
+    _usPerStep = usPerStep;
+
+    _updateStepperSpeed();
+}
+
+void MotorDriverModulo::_updateStepperSpeed() {
+    int microsteps = _microsteps;
+
+    // Can do at most 256 microsteps/step
+    if (microsteps > 256) {
+        microsteps = 256;
+    }
+
+    // If necessary, decrease the number of microsteps to keep the microstep
+    // duration above minMicrostepDuration.
+    while (microsteps > 1 and _usPerStep/microsteps < _minMicrostepDuration) {
+/*
+        Serial.print("Decreasing microsteps from ");
+        Serial.print(microsteps);
+        Serial.print(" to ");
+        Serial.println(microsteps/2);
+*/
+        microsteps /= 2;
+    }
+
+    // Determine the microstep resolution.
+    // It's the base 2 logarithm of microsteps.
+    uint8_t resolution = 0;
+    for (int i = microsteps/2 ; i > 0 and resolution <= 8; i /= 2) {
+        resolution++;
+    }
+
+    // Determine the number of 8us ticks per microstep
+    uint32_t ticksPerMicrostep = _usPerStep/(8*microsteps);
+    
+    // Clamp ticksPerMicrostep to 16 bits
+    if (ticksPerMicrostep > 65535) {
+        ticksPerMicrostep = 65535;
+    }
+    
+/*
+    Serial.print(ticksPerMicrostep*8);
+    Serial.print(" ");
+    Serial.println(microsteps);
+*/
+
+    uint8_t sendData[] = {
+        static_cast<uint8_t>(ticksPerMicrostep),
+        static_cast<uint8_t>(ticksPerMicrostep >> 8),
+        static_cast<uint8_t>(resolution)
+    };
+
+    _transfer(_FunctionSetStepperSpeed, sendData, 3, 0, 0);
+
+}
+
+int32_t MotorDriverModulo::getStepperPosition() {
+    uint8_t receivedData[4];
+    uint32_t pos = 0;
+    if (_transfer(_FunctionGetStepperPosition, 0, 0, receivedData, 4)) {
+        for (int i=3; i >= 0; i--) {
+            pos <<= 8;
+            pos |= receivedData[i];
+        }
+    }
+    return pos;
+
+}
+
+void MotorDriverModulo::setStepperTarget(int32_t targetPosition) {
+    uint8_t sendData[4] = {
+        static_cast<uint8_t>(targetPosition),
+        static_cast<uint8_t>(targetPosition >> 8),
+        static_cast<uint8_t>(targetPosition >> 16),
+        static_cast<uint8_t>(targetPosition >> 24)};
+
+    _transfer(_FunctionSetStepperTarget, sendData, 4, 0, 0);
+}
+
+void MotorDriverModulo::setPWMFrequency(uint16_t frequency) {
+    uint8_t sendData[2] = {
+        static_cast<uint8_t>(frequency),
+        static_cast<uint8_t>(frequency >> 8)};
+
+    _transfer(_FunctionSetStepperTarget, sendData, 2, 0, 0);
+}
+
+void MotorDriverModulo::_processEvent(uint8_t eventCode, uint16_t eventData) {
+    if (eventCode == _EventPositionReached) {
+        if (_positionReachedCallback) {
+            _positionReachedCallback(*this);
+        }
+    }
+
+    if (eventCode == _EventFaultChanged) {
+        if (eventData & 1) {
+            _fault = true;
+        }
+        if (eventData & 2) {
+            _fault = false;
+        }
+
+        if (_faultChangedCallback) {
+            _faultChangedCallback(*this);
+        }
+    }
+}
+
+void MotorDriverModulo::setPositionReachedCallback(EventCallback *handler) {
+    _positionReachedCallback = handler;
+}
+
+/// Set the function that should be called when a fault occurs or is cleared
+void MotorDriverModulo::setFaultChangedCallback(EventCallback *handler) {
+    _faultChangedCallback = handler;
+}
+
--- firmware/MotorDriverModulo.h
+++ firmware/MotorDriverModulo.h
@@ -7,28 +7,98 @@
 /// A driver for DC Motors, Stepper Motor, and other high current devices.
 class MotorDriverModulo : public BaseModulo {
 public:
-    /// Use the first Motor  that's not already in use
+    enum Mode {
+        ModeDisabled, // Output drivers are disconnected
+        ModeDC,       // Drives two DC motors or 4 independent DC channels.
+        ModeStepper   // Drives a stepper motor
+    };
+
+    /// Use the first MotorDriverModulo that's not already in use.
     MotorDriverModulo();
 
-    /// Use the Motor with the provided deviceID
+    /// Use the Motor with the provided deviceID.
     MotorDriverModulo(uint16_t deviceID);
 
     /// Set a single channel (0-3) to the specified amount, between 0 and 1.
+    /// Changes the mode to ModeDC if it's not already.
     void setChannel(uint8_t channel, float amount);
     
     /// Set the motor output A to the specified amount, between -1 and 1.
-    /// Enables A0 and A1 if necessary.
+    /// Changes the mode to ModeDC if it's not already.
     void setMotorA(float amount);
     
     /// Set the motor output B to the specified amount, between -1 and 1.
-    /// Enables B0 and B1 if necessary.
+    /// Changes the mode to ModeDC if it's not already.
     void setMotorB(float amount);
     
-    /// Set whether output drivers are enabled
-    void setEnable(bool enable);
+    /// Set the driver mode.
+    void setMode(Mode mode);
 
     /// Set the driver curren limit (between 0 and 1).
     void setCurrentLimit(float limit);
+
+    /// Set the motor driver PWM frequency
+    void setPWMFrequency(uint16_t frequency);
+
+    /// Set the stepper speed in whole steps per second.
+    void setStepperSpeed(float stepsPerSecond);
+
+    /// Set the number of microseconds to take between each whole step.
+    void setStepperRate(uint32_t usPerStep);
+
+    /// Set the number of microsteps to take between each whole step.
+    /// It can be 1, 2, 4, 8, 16, 32, 64, 128, or 256.
+    /// 
+    /// If the duration of a microstep (in microseconds) would be less than
+    /// minMicrostepDuration, then the number of microsteps is decreased
+    /// automatically. This helps to avoid skipping steps when the rate is
+    /// higher than the motor or driver can handle.
+    void setStepperResolution(int microsteps,
+        uint16_t minMicrostepDuration=1000);
+
+
+    /// Set the stepper target position. The target position is in 1/256 of
+    /// a whole step. (So setting the target to 256 will take as many steps/
+    /// microsteps as are necessary to move to the position that is 1 whole
+    /// step from the starting position.)
+    void setStepperTarget(int32_t targetPosition);
+
+    /// Return the current position of the stepper motor in 1/256 increments
+    /// of wholes steps.
+    int32_t getStepperPosition();
+
+    /// Return whether the stepper driver is currently stepping and has not
+    /// yet reached its target position.
+    bool isStepping();
+
+    bool hasFault();
+
+    /// A callback function
+    typedef void (EventCallback)(MotorDriverModulo &module);
+
+    /// Set the function that should be called when the stepper target position
+    /// is reached.
+    void setPositionReachedCallback(EventCallback *handler);
+
+    /// Set the function that should be called when a fault occurs or is cleared
+    void setFaultChangedCallback(EventCallback *callback);
+
+private:
+    void _updateStepperSpeed();
+    virtual void _processEvent(uint8_t eventCode, uint16_t eventData);
+    
+
+    bool _fault;
+
+    uint32_t _stepperOffset;
+
+    uint32_t _usPerStep;
+    int _microsteps;
+    uint16_t _minMicrostepDuration;
+    EventCallback *_positionReachedCallback;
+    EventCallback *_faultChangedCallback;
+
+
 };
 
 #endif
